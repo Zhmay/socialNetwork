ef12087e73b014e179ca02d3d4fce296007727d7
bogdan <bogdanzhmay@gmail.com>
2025-10-09 11:47:39 +0300

    feat: add @vercel/analytics dependency and integrate it into main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
 import './assets/css/main.css'
+import { inject } from '@vercel/analytics'
 
 const app = createApp(App)
 
@@ -12,3 +11,4 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+inject()
